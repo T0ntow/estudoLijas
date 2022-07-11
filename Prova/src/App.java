ec5dd4b5cd15fe787d3d0c8301d520dd8a9fe18a
Wellington Batista <wellingtonbs109@gmail.com>
2022-07-10 21:43:11 -0300

    versao1.0

--- Prova/src/App.java
+++ Prova/src/App.java
@@ -1,38 +1,72 @@
 import java.util.Scanner;
 
 class Prova {
+  static Scanner entrada = new Scanner(System.in);
+
+  static int matA[][] = new int[3][2], pesq, pesq2; // Variáveis Globais ⬇
+  static int matB[][] = new int[2][2];
+  static int matC[][] = new int[matA.length][matB[0].length];
+
+  static String answer;
+
+  public static void matC() { // Sub Rotina Matriz C
+    System.out.println("A Multiplicação das matrizes A e B é: ");
+
+    for (int i = 0; i < matA.length; i++) {
+      for (int j = 0; j < matB[0].length; j++) {
+        for (int k = 0; k < matB.length; k++) {
+          matC[i][j] += matA[i][k] * matB[k][j];
+        }  
+      }
+    }
+    
+    for (int i = 0; i < matC.length; i++) {
+      for (int j = 0; j < matC[0].length; j++) {
+        System.out.println("linha " + (i + 1) + ", coluna " + (j + 1) + ": " + matC[i][j]);
+      }
+    }
+  } 
+
   public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
 
-    Double num[][] = new Double[2][2], pesq;
-
-    String answer;
-
-    for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 2; j++) {
-        System.out.println("Digite o nome linha: " + (i + 1) + ", coluna: " + (j + 1));
-        num[i][j] = entrada.nextDouble();
+    // MATRIZ A
+    System.out.println("PRIMEIRA MATRIZ");
+    for (int i = 0; i < matA.length; i++) {
+      for (int j = 0; j < matA[i].length; j++) {
+        System.out.println("Digite o número linha: " + (i + 1) + ", coluna: " + (j + 1));
+        matA[i][j] = entrada.nextInt();
       }
     }
 
-    System.out.print("Deseja pesquisar pelos nomes? S/N:");
-    answer = entrada.nextLine();
-
-    while (answer.equalsIgnoreCase("S")) {
-      System.out.print("Digite ai ent: ");
-      pesq = entrada.nextDouble();
-
-      for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-          if (pesq.equals(num[i][j])) {
-            System.out.println(pesq + " foi encontrado, na linha" + (i + 1) + ", coluna: " + (j + 1));
-          }
-        }
+    System.out.println("A matriz é dada por: "); // Saida dos numeros dados
+    for (int i = 0; i < matA.length; i++) {
+      for (int j = 0; j < matA[i].length; j++) {
+        System.out.println(matA[i][j] + " na linha " + (i + 1) + ", coluna: " + (j + 1));
       }
-      System.out.print("Deseja continuar pesquisando nomes? S/N:");
-      answer = entrada.nextLine();
     }
 
+    // MATRIZ B
+
+    System.out.println("SEGUNDA MATRIZ"); // k=linha /l=coluna
+    for (int i = 0; i < matB.length; i++) {
+      for (int j = 0; j < matB[i].length; j++) {
+        System.out.println("Digite o número linha: " + (i + 1) + ", coluna: " + (j + 1));
+        matB[i][j] = entrada.nextInt();
+      }
+    }
+
+    System.out.println("A matriz é dada por: "); // Saida dos numeros dados
+    for (int i = 0; i < matB.length; i++) {
+      for (int j = 0; j < matB[i].length; j++) {
+        System.out.println(matB[i][j] + " na linha " + (i + 1) + ", coluna: " + (j + 1));
+      }
+    }
+
+    System.out.print("Deseja multiplicar as matrizes? S/N:");
+    answer = entrada.next();
+
+    if (answer.equalsIgnoreCase("S")) {
+      matC();
+    }
   }
-
 }
